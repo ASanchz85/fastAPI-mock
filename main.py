0a733d59c348e52326d870105363cbfec824260d
Antonio S <asanchz85@gmail.com>
2024-04-07 17:40:51 +0200

    adding status codes to endpoints

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
 from random import randrange
 
@@ -19,48 +19,55 @@ my_posts = [
         "content": "Just here to test API functionality",
         "published": True,
         "rating": 5,
-        "id": 1
+        "id": 1,
     },
     {
         "title": "second post as an example",
         "content": "another simple post to test the API",
         "published": True,
         "rating": 3,
-        "id": 2
-    }
+        "id": 2,
+    },
 ]
 
 
 @app.get("/")
 async def root():
-    return {"message": "welcome to our API, visit the link below to get access to the documentation", "docs_link": "http://localhost:8000/docs", "error": None}
+    return {
+        "message": "welcome to our API, visit the link below to get access to the documentation",
+        "docs_link": "http://localhost:8000/docs",
+        "error": None,
+    }
 
-    
+
 @app.get("/posts")
 async def get_posts():
     return {"message": "Successfully requested", "data": my_posts, "error": None}
 
 
 @app.post("/posts")
-async def create_post(new_post: Post):
+async def create_post(new_post: Post, response: Response):
     # !deprecated >> my_posts.append(new_post.dict())
     post_dict = new_post.__dict__
     post_dict["id"] = randrange(0, 100000)
     my_posts.append(post_dict)
-    print(my_posts)
+    response.status_code = status.HTTP_201_CREATED
     return {"message": "Successfully created post", "data": post_dict, "error": None}
 
 
 # !carefull with the order of the routes, because the one with the variable can't be the first one
 @app.get("/posts/latest")
-async def get_latest_post():
+async def get_latest_post(response: Response):
     latest_post = my_posts[-1]
+    response.status_code = status.HTTP_200_OK
     return {"message": "Successfully requested", "data": latest_post, "error": None}
 
 
 @app.get("/posts/{post_id}")
-async def get_single_post(post_id: int):
+async def get_single_post(post_id: int, response: Response):
     for post in my_posts:
         if post["id"] == post_id:
+            response.status_code = status.HTTP_200_OK
             return {"message": "Successfully requested", "data": post, "error": None}
+    response.status_code = status.HTTP_404_NOT_FOUND
     return {"message": "Post not found", "data": None, "error": "Post not found"}
